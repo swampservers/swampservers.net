e257c6e6ff0eaec89bc1e159e13f28dcdd0c35a0
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-05-15 17:12:48 +0800

    Update index.php

--- help/index.php
+++ help/index.php
@@ -3,7 +3,6 @@
 	<meta charset="utf-8">
 	<title>Swamp Servers - Commands</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link href='https://fonts.googleapis.com/css?family=Righteous' rel='stylesheet'>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style type="text/css">
 		*{
