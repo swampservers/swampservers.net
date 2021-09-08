66ed0d0dfe1c8e440f04095f016cdc47e1725c7e
Swamp <swampservers@gmail.com>
2021-09-08 15:33:50 -0500

    scss reorg

--- scss/compile.sh
+++ scss/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sass ./scss/bootstrap.scss ./css/bootstrap.min.css
-sass ./scss/style.scss ./css/style.css
+# sass ./scss/bootstrap.scss ./css/bootstrap.min.css
+sass ./scss/style.scss ./css/style.min.css
