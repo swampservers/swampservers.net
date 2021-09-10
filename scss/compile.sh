72c426b4180bb929b276cfd26ef21082f47ee262
Swamp <swampservers@gmail.com>
2021-09-10 15:37:52 -0500

    update

--- scss/compile.sh
+++ scss/compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # sass ./scss/bootstrap.scss ./css/bootstrap.min.css
 sass ./scss/style.scss ./css/style.min.css
+sass ./scss/style_awesomium.scss ./css/style_awesomium_screw_you_garry.min.css
+# sass ./scss/assets/stylesheets/_bootstrap.scss ./css/style.min.css
