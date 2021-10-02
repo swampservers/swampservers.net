16546ba1f5834bcb737fd12b62a07725900aa511
floppaa <shadyhoodlum@protonmail.com>
2021-10-02 17:23:04 -0400

    Landing page updates

--- scss/compile.sh
+++ scss/compile.sh
@@ -2,4 +2,5 @@
 # sass ./scss/bootstrap.scss ./css/bootstrap.min.css
 sass ./scss/style.scss ./css/style.min.css
 sass ./scss/style_awesomium.scss ./css/style_awesomium_screw_you_garry.min.css
+sass ./scss/landing_page.scss ./css/landing_page.min.css
 # sass ./scss/assets/stylesheets/_bootstrap.scss ./css/style.min.css
