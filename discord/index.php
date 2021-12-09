8fbc5a15ab11af1d82ec8ec0445aeb12a0a6f18a
Swamp <swampservers@gmail.com>
2021-12-09 09:34:00 -0600

    update

--- discord/index.php
+++ discord/index.php
@@ -7,8 +7,10 @@
 
 <p>We do have a Discord guild as well, but we only use it for voice/video chat. The link can be found in the "VOICE" section of our Steam Chat.<br>&nbsp;</p>
 
-<p>The reason we avoid Discord is because of their <a href="/discordmoderators">endless unreasonable takedowns</a> of our accounts and groups.<br>&nbsp;</p>
-
+<p>The reason we avoid Discord is because of their endless unreasonable takedowns. We can't run a Discord because any time anybody says anything even slightly offensive, we have to be paranoid that it will result in our group (and personal accounts) being banned for the 10th time, despite our best attempts to keep our group in compliance with their ToS.<br>&nbsp;</p>
+<!-- <br>&nbsp; -->
+<!-- <a href="/discordmoderators"> -->
+<!-- of our accounts and groups -->
 </div>
 
 
